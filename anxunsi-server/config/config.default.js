c52a7694838f70947ef2fb3127d07b9acd9adc07
且吟且行 <admin@washmore.tech>
2018-11-28 18:45:43 +0800

    测试环境的数据库连接放到默认配置文件;

--- anxunsi-server/config/config.default.js
+++ anxunsi-server/config/config.default.js
@@ -10,7 +10,19 @@ module.exports = appInfo => {
         // },
         root: path.join(appInfo.baseDir, '/app/'),
     };
-
+    config.sequelize = {
+        // support: mysql, mariadb, postgres, mssql
+        dialect: 'mysql',
+        database: 'anxunsi',
+        host: 'localhost',
+        port: '3306',
+        username: 'root',
+        password: '',
+        // delegate: 'myModel', // load all models to `app[delegate]` and `ctx[delegate]`, default to `model`
+        // baseDir: 'my_model', // load all files in `app/${baseDir}` as models, default to `model`
+        // exclude: 'index.js', // ignore `app/${baseDir}/index.js` when load models, support glob and array
+        // more sequelize options
+    };
     // use for cookie sign key, should change to your own and keep security
     config.keys = appInfo.name + '_anxunsi_token';
 
