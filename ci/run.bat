850da95f9e80b37d349851410dd08ebb9c0eb087
且吟且行 <admin@washmore.tech>
2018-12-13 22:17:42 +0800

    win 一键启动
    washmore

--- ci/run.bat
+++ ci/run.bat
@@ -1,5 +1,5 @@
-npm install -g cnpm --registry=https://registry.npm.taobao.org
+call npm config set registry https://registry.npm.taobao.org
 cd anxunsi-client
-call cnpm i && call cnpm run build
+call npm i && call npm run build
 cd ..\anxunsi-server
-call cnpm i && call cnpm run dev
+call npm i && call npm run dev
